9661b7cad11675f94cfb2f6805614528bc8d10da
MDecker-MobileComputing <mide_42b@yahoo.de>
2024-01-26 10:21:58 +0100

    .gitignore und implementierung zwei Step-Methoden dazu

--- src/test/java/de/eldecker/dhbw/cucumber/stringtest/StepDefinitions.java
+++ src/test/java/de/eldecker/dhbw/cucumber/stringtest/StepDefinitions.java
@@ -1,17 +1,43 @@
 package de.eldecker.dhbw.cucumber.stringtest;
 
-import io.cucumber.java.en.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.Assertions.*;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
 
 public class StepDefinitions {
 
     private String _derString = null;
 
+
     @Given("der String ist {string}")
     public void givenDerStringIst(String str) {
 
         _derString = str;
+    }
+
+
+    /**
+     * Step-Methode für Variante 1: int-Wert mit String-Länge
+     * ist im Satz nicht in spezielle Zeichen gesetzt.
+     */
+    @Then("ist die String-Länge {int}")
+    public void thenIstDieStringLaenge(int laenge) {
+
+            assertEquals(laenge, _derString.length());
+    }
+
+
+    /**
+     * Step-Methode für Variante 2: int-Wert mit String-Länge
+     * ist im Satz in spitze Klammern gesetzt um sich klar zu
+     * machen, dass es sich hierbei um eine Variable handelt.
+     */
+    @Then("ist die Länge des Strings <{int}>")
+    public void thenIstDieLaengeDesStrings(int laenge) {
+
+            assertEquals(laenge, _derString.length());
     }
 
 }
